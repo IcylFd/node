018d58fa023f2af439ddda04637a53388b0df680
lifangdi <772167928@qq.com>
2018-09-12 21:42:34 +0800

    2018.9.12 node

--- filelist.js
+++ filelist.js
@@ -2,23 +2,32 @@ var fs = require('fs');
 var http = require('http');
 
 http.createServer(function (req, res) {
-    fs.readdir('dir',function (err,files) {
+    readDirName(__dirname,req,res);
+}).listen(3000);
+
+function readDirName(dirname,req,res) {
+    fs.readdir(dirname,function (err,files) {
         if(err){
             res.writeHead(404,{'Content-type':'plain'});
             res.end('not found');
         }else{
-            console.log(files);
-            fs.stat('dir',function (err,stats) {
-                if(err){
-                    res.writeHead(404,{'Content-type':'plain'});
-                    res.end('not found');
-                }
-                else if(stats.isDirectory()) {
-                    
-                }
-            })
+            for(var i = 0; i < files.length; i++) {
+                fs.stat(files[i],function (err,stats) {
+                    if(err){
+                        res.writeHead(404,{'Content-type':'plain'});
+                        res.end('not found');
+                    } else if(stats.isDirectory()) {
+                        readDirName(files[i],req,res);
+                        console.log(files[i]);
+                    } else{
+                        console.log(files[i]);
+                    }
+                })
+            }
+
         }
     })
-}).listen(3000);
+}
+
 
 console.log('server start');
