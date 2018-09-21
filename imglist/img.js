5c3e903f1057e768ba70a9024ce26ea15d5c94ec
lifangdi <772167928@qq.com>
2018-09-21 20:28:46 +0800

    2018.9.21 node

--- imglist/img.js
+++ imglist/img.js
@@ -2,6 +2,21 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
-http.createServer = function (res,req) {
-    var pathname = url.parse()
+http.createServer(function (res,req) {
+    // var pathname = url.parse(req.url).pathname;
+    console.log(req.url);
+    // switch(pathname){
+    //     case '/img':
+    //         getImg(res,req);
+    //         break;
+    // }
+}).listen(3000);
+
+function getImg(res,req) {
+    var pathname = __dirname+'/img'+url.parse('123.jpg').pathname;
+    var imgData = fs.readFileSync(pathname);
+    res.writeHead(200,{"Content-type":"text/jpeg"});
+    res.end(imgData);
 }
+
+console.log("server start");
--- jingtai/http.js
+++ jingtai/http.js
@@ -13,6 +13,50 @@ http.createServer(function (req, res) {
     }else if(pathname == '/' || pathname == "/index"){
         goIndex(res);
     }else{
-        dealWithStatic();
+        dealWithStatic(pathname, realPath, res);
     }
-})
+}).listen(3000);
+
+function goIndex(res) {
+    var readPath = __dirname + "/" + url.parse('index.html').pathname;
+    var indexPage = fs.readFile(readPath);
+    res.writeHead(200,{"Content-type":"text/html"});
+    res.end(indexPage);
+}
+
+function dealWithStatic(pathname, realPath,res){
+    fs.exists(realPath,function (exists) {
+        if(!exists){
+            res.writeHead(404,{"Content-type":"text/plain"});
+            res.write("the requset url "+pathname+"is wrong");
+            res.end();
+        }else{
+            var pointPosition = pathname.lastIndexOf(realPath);
+            var mmieString = pathname.substring(pointPosition);
+            var mmieType;
+            switch(mmieString){
+                case 'css':
+                    mmieType = "text/css";
+                    break;
+                case 'jpg':
+                    mmieType = "text/jpeg";
+                    break;
+                default:
+                    mmieType = "text/plain";
+                    break;
+            }
+        }
+        fs.readFile(realPath,"binary",function (err,file) {
+            if(err){
+                res.writeHead(500,{"Content-type":"text/plain"});
+                res.end(err);
+            }else{
+                res.writeHead(200,{"Content-type":mmieType});
+                res.write(file,"binary");
+                res.end();
+            }
+        })
+    })
+}
+
+console.log("server start");
